b539b8a0e349434c0d8f25e0bf483e5ffc06f6e6
Chregu <christoph.mueller90@protonmail.com>
2024-09-29 09:40:46 +0200

    Rail fixes

--- Assets/Scripts/Obstacles/ModRail.cs
+++ Assets/Scripts/Obstacles/ModRail.cs
@@ -21,18 +21,26 @@ public class ModRail : MonoBehaviour
         {
             for (int i = 0; i < railNodes; i++)
             {
-                Spawn(((float)i + 1) / ((float)railNodes + 1));
+                Spawn(((float)i + 1) / ((float)railNodes + 1), i);
             }
         }
     }
 
 
-    void Spawn(float t)
+    void Spawn(float t, int index)
     {
         GameObject railNode = new GameObject();
         railNode.tag = "RailNode";
         railNode.transform.SetParent(transform);
         railNode.transform.position = curve.GetPoint(t);
         railNode.transform.forward = curve.GetDirection(t);
+        if (index == 0)
+        {
+            railNode.transform.position = curve.GetPoint(0);
+        }
+        else if (index == railNodes - 1)
+        {
+            railNode.transform.position = curve.GetPoint(railNodes - 1);
+        }
     }
 }
