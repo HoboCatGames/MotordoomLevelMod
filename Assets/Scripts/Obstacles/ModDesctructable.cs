7ccd8dce437fdbf90e47a1dee56585ebc05140bb
Chregu <christoph.mueller90@protonmail.com>
2024-10-06 11:47:24 +0200

    Added Jump Pads and Destructables

---
+++ Assets/Scripts/Obstacles/ModDesctructable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ModDesctructable : MonoBehaviour
+{
+    public float health;
+    public bool destroyOnTouch = false;
+    public ParticleSystem destructionParticles;
+    public GameObject meshToDestroy;
+}
---
+++ Assets/Scripts/Obstacles/ModJumpPad.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class ModJumpPad : MonoBehaviour
+{
+    public Vector3 force;
+}
